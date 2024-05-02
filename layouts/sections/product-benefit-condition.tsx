a4c9b7c4d4869c45b0521e7729a32bf35d71702f
netwpth <wipawee_tha@hotmail.com>
2024-05-02 16:21:29 +0700

    product

--- layouts/sections/product-benefit-condition.tsx
+++ layouts/sections/product-benefit-condition.tsx
@@ -6,9 +6,30 @@ html_benefit_text check is "" not display    หัวข้อ  -> เงื�
 html_condition check is "" not display 
 */
   return (
-    <div>
-      <p className="text-3xl font-medium text-primary">ผลประโยชน์และความคุ้มครอง</p>
-      <p className="text-lg font-normal text-gray-500">ผลประโยชน์และความคุ้มครอง</p>
+    <div className="p-8 width-description max-w-screen-xl">
+      { product.html_benefit_text !== "" &&
+        <p className="text-3xl font-medium text-primary">ผลประโยชน์และความคุ้มครอง</p>
+      }
+      <ul>
+        <li className="text-lg font-normal text-gray-500"
+        dangerouslySetInnerHTML={{__html: product.html_benefit_text}}
+        >
+        </li>
+      </ul>
+
+     
+      { product.html_benefit_text !== "" &&
+        <p className="text-3xl font-medium text-primary">เงื่อนไขประกัน</p>
+      }
+      <ul>
+        <li className="text-lg font-normal text-gray-500"
+        dangerouslySetInnerHTML={{__html: product.html_condition}}
+        >
+        </li>
+      </ul>
     </div>
   )
 } 
+
+
+
--- layouts/sections/product-remark.tsx
+++ layouts/sections/product-remark.tsx
@@ -5,14 +5,12 @@ export const ProductRemark = ({ product }: { product: Product }) => {
 html_disclaimer check is ""  -> not display แปลงhtml กับ check ค่าว่าง
 */
   return (
-    <div className="p-8">
+    <div className="p-8 width-description max-w-screen-xl">
       <ul className="list-inside list-style">
         <li className="text-sm font-normal text-gray-500"
         dangerouslySetInnerHTML={{__html: product.html_disclaimer}}
         >
         </li>
-        
-        
       </ul>
     </div>
   )
