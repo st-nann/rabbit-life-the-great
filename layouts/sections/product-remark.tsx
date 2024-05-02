649f562edba902cfebdb73f622dec28971b0060c
netwpth <wipawee_tha@hotmail.com>
2024-05-02 13:08:56 +0700

    product-remark

---
+++ layouts/sections/product-remark.tsx
@@ -0,0 +1,19 @@
+import { Product } from "@/types/product"
+
+export const ProductRemark = ({ product }: { product: Product }) => {
+/*
+html_disclaimer check is ""  -> not display แปลงhtml กับ check ค่าว่าง
+*/
+  return (
+    <div className="p-8">
+      <ul className="list-inside list-style">
+        <li className="text-sm font-normal text-gray-500"
+        dangerouslySetInnerHTML={{__html: product.html_disclaimer}}
+        >
+        </li>
+        
+        
+      </ul>
+    </div>
+  )
+} 
