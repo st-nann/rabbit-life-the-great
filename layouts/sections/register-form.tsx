51e50779a94bdd9efdf40817483fd5d34c754158
st.nann <st.sanyanee@hotmail.com>
2024-05-02 13:53:17 +0700

    form layout

--- layouts/sections/register-form.tsx
+++ layouts/sections/register-form.tsx
@@ -1,5 +1,6 @@
 export const RegisterForm = () => {
   return (
-    <div></div>
+    <div className="flex flex-wrap flex-row justify-center content-center bg-slate-100 p-8 w-full gap-10 max-w-screen-xl rounded-xl">
+    </div>
   )
 }
