45112cad28c73c173482b7ca64b197593e068c7a
st.nann <st.sanyanee@hotmail.com>
2024-05-02 13:48:51 +0700

    html disclaimer

--- layouts/sections/product-remark.tsx
+++ layouts/sections/product-remark.tsx
@@ -1,19 +1,17 @@
 import { Product } from "@/types/product"
+import { convertHtmlDisclaimer } from "@/utils/products"
+import get from "lodash/get"
 
 export const ProductRemark = ({ product }: { product: Product }) => {
 /*
 html_disclaimer check is ""  -> not display แปลงhtml กับ check ค่าว่าง
 */
+  const htmlDisclaimer = get(product, "html_disclaimer", "")
+  const htmlDisclaimerReplace = convertHtmlDisclaimer(htmlDisclaimer)
+
   return (
-    <div className="p-8">
-      <ul className="list-inside list-style">
-        <li className="text-sm font-normal text-gray-500"
-        dangerouslySetInnerHTML={{__html: product.html_disclaimer}}
-        >
-        </li>
-        
-        
-      </ul>
+    <div className="flex flex-wrap flex-row p-8 max-w-screen-xl">
+     <div dangerouslySetInnerHTML={{ __html: htmlDisclaimerReplace }} />
     </div>
   )
 } 
